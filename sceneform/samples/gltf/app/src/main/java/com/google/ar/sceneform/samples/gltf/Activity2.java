c70325953c4c9d98eb38f0c35fbf41b5d1657b06
einceoglu20 <einceoglu20@ku.edu.tr>
2022-08-02 12:40:03 +0300

    deleted unneccessary files

--- sceneform/samples/gltf/app/src/main/java/com/google/ar/sceneform/samples/gltf/Activity2.java
+++ sceneform/samples/gltf/app/src/main/java/com/google/ar/sceneform/samples/gltf/Activity2.java
@@ -2,11 +2,10 @@ package com.google.ar.sceneform.samples.gltf;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
